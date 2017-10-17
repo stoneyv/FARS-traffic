56626752d2a18dab5d91d22ca2ddacf39124f44f
Stoney Vintson <stoneyv@gmail.com>
2017-10-17 13:36:05 -0700

    download US state and county shapefiles

--- download-data-fars.sh
+++ download-data-fars.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-# Run this from the top directory FARS-traffic
+# Run this from the directory FARS-traffic
 
 # Check for wget and unzip
 command -v wget >/dev/null 2>&1 || { echo "wget not installed.  Aborting." >&2; exit 1; }
 command -v unzip >/dev/null 2>&1 || { echo "unzip not installed.  Aborting." >&2; exit 1; }
 
 # Make directories for each year
-mkdir -p ./data/{2010..2015}
+if [ ! -e ./data/2010 ]; then
+  mkdir -p ./data/{2010..2015}
+fi
 
-base_url_fars="ftp://ftp.nhtsa.dot.gov/FARS"
 
 # List of NHTA FARS ftp URLs for years 2010 to 2015
+base_url_fars="ftp://ftp.nhtsa.dot.gov/FARS"
 declare -a fars_urls=( "2010/SAS/FSAS2010.zip"
                        "2011/SAS/FSAS2011.zip"
                        "2012/National/SAS/FSAS2012.zip"
@@ -31,4 +33,41 @@ for url in "${fars_urls[@]}"; do
   rm $file
 done
 
+# GSA Geographic Location Codes used to encode US Cities, Counties, etc
+# FARS data manual pages 38, 39 (table of contents off by one page)
+wget "https://www.gsa.gov/cdnstatic/GLCs_for_the_USA_and_DC_%281%29.xlsx"
+mv GLCs_for_the_USA_and_DC_\(1\).xlsx GLCs_for_the_USA_and_DC.xlsx
+
+# Return to the previous directory
+popd
+
+
+## Download Census 2015 US boundary shapefiles ##
+## State, County                               ##
+base_url_census_tiger="http://www2.census.gov/geo/tiger"
+declare -a tiger_urls=("GENZ2015/shp/cb_2015_us_state_20m.zip"
+                       "GENZ2015/shp/cb_2015_us_county_20m.zip")
+
+if [ ! -e ./data/shapefiles ]; then
+  mkdir -p ./data/shapefiles
+fi
+
+# Enter the ./data/shapefiles directory
+pushd ./data/shapefiles
+
+for shapefile in "${tiger_urls[@]}"; do
+  wget ${base_url_census_tiger}/$shapefile
+done
+
+# Unzip all of the shapefiles into their own directory
+# and remove the zip file.
+zipfile_arr=(*.zip)
+for file in "${zipfile_arr[@]}"; do
+  base=${file%%.*}
+  mkdir $base
+  unzip -d $base $file
+  rm $file
+done
+
+# Return to the main directory
 popd
